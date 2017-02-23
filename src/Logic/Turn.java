cec40b8d7d64d6ad69bb358dbd444790face3de7
piing3 <d.holmberg@sjasd.ca>
2017-02-23 10:50:10 -0600

    MiniClean

--- src/Logic/Turn.java
+++ src/Logic/Turn.java
@@ -1,20 +1,39 @@
 package Logic;
 
+import java.util.ArrayList;
+
 /**
- *
+ * 
  * @author d.holmberg
  */
 public class Turn {
 
+    private int maxSize = 4;
+    private int turn = 0;
+    private Player[] players = new Player[maxSize];
 
+    public void addPlayer(Player newPlayer){
+        for (int i = 0; i < maxSize; i++) {
+            if(players[i] == null) players[i] = newPlayer;
+        }
+    }
 
 
     //----Variables-----------
 
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public int getTurn() {
+        return turn;
+    }
+    public void setTurn(int turn) {
+        this.turn = turn;
+    }
 
     
     //----Object-Methods-------
-
     @Override
     protected Object clone() throws CloneNotSupportedException {
         return super.clone();
